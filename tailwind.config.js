7a2ca18f76c866b49bc28465f1a5832897e16c5b
arafat709 <mdarafat112007@gmail.com>
2024-11-30 11:09:06 +0600

    second

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      padding:"50px"
+    },
+    extend: {
+      backgroundImage: {
+        "hero":"url('/src/assets/hero.png')",
+        "subs":"url('/src/assets/subs.png')",
+      },
+      backgroundColor: {
+        "subscolor":"#FF7D68"
+      },
+    },
+  },
+  plugins: [],
+}
